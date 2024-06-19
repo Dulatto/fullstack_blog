8e2877cfa80a5709a5ed89fc19b7f1c95d13e25f
Dulatto <dulat.omarov@gmail.com>
2024-06-18 22:10:09 -0600

    validation

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose'
+import { validationResult } from 'express-validator'
+
+import { registerValidation } from './validations/auth.js'
 
 mongoose
     .connect('mongodb+srv://dukaomarrich:013309do@cluster0.aenh8u7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
@@ -12,22 +15,15 @@ const app = express();
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
 
-app.post('/auth/login', (req, res) => {
-    console.log(req.body)
-
-    const token = jwt.sign({
-        email: req.body.email,
-        fullName: 'Vasia Pupkin'
-    },
-        'secret123');
+app.post('/auth/registr', registerValidation, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array())
+    }
 
     res.json({
-        success: true,
-        token
+        success: true
     })
 })
 
--- models/User.js
+++ models/User.js
@@ -1,21 +1,22 @@
 import mongoose from 'mongoose'
 
-const UserSchema = new mongoose.Schema({
-    fullName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    passwordHash: {
-        type: String,
-        required: true
-    },
-    avatarUrl: String
-}, {
+const UserSchema = new mongoose.Schema(
+    {
+        fullName: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        passwordHash: {
+            type: String,
+            required: true
+        },
+        avatarUrl: String
+    }, {
     timestamps: true,
 })
 
