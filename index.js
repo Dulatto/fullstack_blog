41f6f28c8f5880d9e2bc72c75ef52b6cc2eced97
Dulatto <dulat.omarov@gmail.com>
2024-06-20 22:37:55 -0600

    bcrypt

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'
 import mongoose from 'mongoose'
 import { validationResult } from 'express-validator'
 
-import { registerValidation } from './validations/auth.js'
+import { registerValidation } from './validations/auth.js';
+
+import UserModel from './models/User.js'
 
 mongoose
     .connect('mongodb+srv://dukaomarrich:013309do@cluster0.aenh8u7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
@@ -16,15 +19,26 @@ const app = express();
 app.use(express.json())
 
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json(errors.array())
     }
 
-    res.json({
-        success: true
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash
     })
+
+    const user = await doc.save();
+
+    res.json(user)
 })
 
 app.listen(4444, (err) => {
