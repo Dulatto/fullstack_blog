367f03f8541887fc8e17c4249aab9a8adaee03a6
Dulatto <dulat.omarov@gmail.com>
2024-06-20 22:49:08 -0600

    code adjusting

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { registerValidation } from './validations/auth.js';
 import UserModel from './models/User.js'
 
 mongoose
-    .connect('mongodb+srv://dukaomarrich:013309do@cluster0.aenh8u7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .connect('mongodb+srv://dukaomarrich:013309do@cluster0.aenh8u7.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => console.log('DB OK'))
     .catch((err) => console.log('DB error', err))
 
@@ -20,25 +20,32 @@ app.use(express.json())
 
 
 app.post('/auth/register', registerValidation, async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array())
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array())
+        }
+
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash
+        })
+
+        const user = await doc.save();
+
+        res.json(user)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Failed to register'
+        })
     }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10)
-    const passwordHash = await bcrypt.hash(password, salt)
-
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash
-    })
-
-    const user = await doc.save();
-
-    res.json(user)
 })
 
 app.listen(4444, (err) => {
