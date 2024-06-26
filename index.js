09e19d8363dd54afe49ed187b7f98a53f5c50bfd
Dulatto <dulat.omarov@gmail.com>
2024-06-25 22:39:39 -0600

    user authorization

--- index.js
+++ index.js
@@ -18,6 +18,45 @@ const app = express();
 
 app.use(express.json())
 
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'The user is not found',
+            });
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+
+        if (!isValidPass) {
+            return res.status(404).json({
+                message: 'Incorrect username or password'
+            })
+        }
+        const token = jwt.sign({
+            _id: user._id
+        },
+            'secret123',
+            {
+                expiresIn: '5000'
+            }
+        )
+
+        const { passwordHash, ...userData } = user._doc
+
+        res.json({
+            ...userData,
+            token
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Failed to login'
+        })
+    }
+})
 
 app.post('/auth/register', registerValidation, async (req, res) => {
     try {
